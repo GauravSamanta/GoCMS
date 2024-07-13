9fc2bac55b18bf6e476e469e563ae404c783cee4
Hrishikesh-Panigrahi <hrishikesh.panigrahi38@gmail.com>
2024-07-13 21:48:31 +0530

    Update Models.go

--- models/Models.go
+++ models/Models.go
@@ -8,9 +8,9 @@ import (
 
 type User struct {
 	gorm.Model
-	Name     string // A regular string field
-	Email    string `json:"email" gorm:"unique,not null"` // A pointer to a string, allowing for null values
-	Password string // A regular stringx`` field
+	Name     string 
+	Email    string `json:"email" gorm:"unique,not null"` 
+	Password string 
 	RoleID   uint   `gorm:"not null;DEFAULT:2" json:"role_id"`
 	Role     Role   `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;" json:"-"`
 }
@@ -23,13 +23,13 @@ type Role struct {
 }
 
 type Post struct {
-	ID            uint           `json:"id" gorm:"primaryKey;autoIncrement"`               // Standard field for the primary key
-	Title         string         `json:"title" binding:"uppercase"`                        // A regular string field
-	Description   string         `json:"description"`                                      // A regular string field
-	Content       string         `json:"content"`                                          // A regular string field
-	CreatedAt     time.Time      `json:"created_at" time_format:"2006-01-02"`              // Standard field for the creation time
-	UpdatedAt     time.Time      `json:"updated_at" time_format:"2006-01-02"`              // Standard field for the update time
-	DeletedAt     gorm.DeletedAt `json:"deleted_at" gorm:"index" time_format:"2006-01-02"` // Standard field for soft delete
+	ID            uint           `json:"id" gorm:"primaryKey;autoIncrement"`               
+	Title         string         `json:"title" binding:"uppercase"`                        
+	Description   string         `json:"description"`                                      
+	Content       string         `json:"content"`                                          
+	CreatedAt     time.Time      `json:"created_at" time_format:"2006-01-02"`              
+	UpdatedAt     time.Time      `json:"updated_at" time_format:"2006-01-02"`              
+	DeletedAt     gorm.DeletedAt `json:"deleted_at" gorm:"index" time_format:"2006-01-02"` 
 	FormattedDate string         `gorm:"-" json:"-"`
 	//relationship with user
 	UserID uint `json:"user_id"`
@@ -37,16 +37,16 @@ type Post struct {
 }
 
 type Image struct {
-	UUID string `json:"uuid"`                               // A regular string field
-	ID   uint   `json:"id" gorm:"primaryKey;autoIncrement"` // Standard field for the primary key
-	Name string `json:"name"`                               // A regular string field
-	Path string `json:"path"`                               // A regular string field
-	Alt  string `json:"alt"`                                // A regular string field
-	// PostID    uint           `json:"post_id"`                            // A regular string field
+	UUID string `json:"uuid"`                               
+	ID   uint   `json:"id" gorm:"primaryKey;autoIncrement"` 
+	Name string `json:"name"`                               
+	Path string `json:"path"`                               
+	Alt  string `json:"alt"`                                
+	// PostID    uint           `json:"post_id"`                            
 	// Post      Post           `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;" json:"-"`
-	CreatedAt time.Time      `json:"created_at" time_format:"2006-01-02"`              // Standard field for the creation time
-	UpdatedAt time.Time      `json:"updated_at" time_format:"2006-01-02"`              // Standard field for the update time
-	DeletedAt gorm.DeletedAt `json:"deleted_at" gorm:"index" time_format:"2006-01-02"` // Standard field for soft delete
+	CreatedAt time.Time      `json:"created_at" time_format:"2006-01-02"`              
+	UpdatedAt time.Time      `json:"updated_at" time_format:"2006-01-02"`              
+	DeletedAt gorm.DeletedAt `json:"deleted_at" gorm:"index" time_format:"2006-01-02"` 
 	//todo relationship with post
 	// PostID uint `json:"post_id"`
 	// Post   Post `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE,foreignKey:PostID;" json:"-"`
